78c9497c767559cf7c4068631a52101b63e641ca
Edmund Hung <me@edmund.dev>
2021-10-13 00:06:25 +0200

    test: createQuery using miniflare

--- test/createQuery.spec.ts
+++ test/createQuery.spec.ts
@@ -1,54 +1,21 @@
+import { Miniflare } from 'miniflare';
+import preview from '../src/commands/preview';
 import createQuery from '../src/createQuery';
 import fixtures from './fixtures.json';
 
-interface KV {
-  key: string;
-  value?: string;
-  metadata?: any;
-}
-
-function parse(value: string | undefined, type: string): unknown | null {
-  if (typeof value === 'undefined') {
-    return null;
-  }
-
-  try {
-    switch (type) {
-      case 'json':
-        return JSON.parse(value);
-      default:
-        return value;
-    }
-  } catch {
-    return null;
-  }
-}
-
-// Simple kv namespace implementation which just covers the usage of createQuery()
-function createNamespace(list: KV[]) {
-  async function getWithMetadata(key: string, type: string) {
-    const kv = list.find(data => data.key === key);
-
-    return {
-      value: parse(kv?.value, type) ?? null,
-      metadata: kv?.metadata ?? null
-    };
-  }
-
-  async function get(key: string, type: string) {
-    const kv = list.find(data => data.key === key);
-
-    return parse(kv?.value, type) ?? null;
-  }
-
-  return {
-    get,
-    getWithMetadata,
-  };
-}
-
 describe('createQuery', () => {
-  const namespace = createNamespace(fixtures);
+  let namespace: KVNamespace;
+
+  beforeAll(async () => {
+    const mf = new Miniflare({
+      script: 'addEventListener("fetch", () => {});',
+      buildCommand: '',
+    });
+
+    await preview(mf, fixtures, 'test');
+
+    namespace = await mf.getKVNamespace('test');
+  });
 
   it('creates 3 different queries', () => {
     const query = createQuery(namespace);
