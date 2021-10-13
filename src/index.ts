adb5dfce9334ad88247dab7405c27789d33c11a3
Edmund Hung <me@edmund.dev>
2021-10-13 19:46:03 +0200

    feat: setup entry file with build script

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import createQuery from "./createQuery";
+import generate from "./commands/generate";
+import preview from "./commands/preview";
+import publish from "./commands/publish";
+
+export {
+  createQuery,
+  generate,
+  preview,
+  publish,
+};
